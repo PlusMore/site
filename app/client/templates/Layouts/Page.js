774e2d67d8b1b524ef50e8efb26eba7d1f2aea6e
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-20 18:29:21 -0500

    table page nav, a lot of work, not worth the effort right now

--- app/client/templates/Layouts/Page.js
+++ app/client/templates/Layouts/Page.js
@@ -2,6 +2,25 @@ Template.Page.rendered = function () {
   App.fullScreenContainer();
 
   this.$('.page').on('scroll', scrollPage);
+
+  this.$('.nav', '#page-nav').onePageNav({
+      currentClass: 'current',
+      changeHash: false,
+      scrollSpeed: 650,
+      scrollOffset: 30,
+      scrollThreshold: 0.5,
+      filter: ':not(.login, .signup)',
+      easing: 'swing',
+      begin: function() {
+          //I get fired when the animation is starting
+      },
+      end: function() {
+          //I get fired when the animation is ending
+      },
+      scrollChange: function($currentListItem) {
+          //I get fired when you enter a section and I pass the list item of the section
+      }
+  });
 };
 
 function _scrollPage(e) {
--- app/client/templates/Layouts/SiteLayout.js
+++ app/client/templates/Layouts/SiteLayout.js
@@ -1,28 +1,3 @@
-function onePageScroll() {
-      $('.nav', '#page-nav').onePageNav({
-          currentClass: 'current',
-          changeHash: false,
-          scrollSpeed: 650,
-          scrollOffset: 30,
-          scrollThreshold: 0.5,
-          filter: ':not(.login, .signup)',
-          easing: 'swing',
-          begin: function() {
-              //I get fired when the animation is starting
-          },
-          end: function() {
-              //I get fired when the animation is ending
-          },
-          scrollChange: function($currentListItem) {
-              //I get fired when you enter a section and I pass the list item of the section
-          }
-      });
-    }
-
-Template.SiteLayout.rendered = function () {
-  onePageScroll();
-};    
-
 Template.SiteLayout.helpers({
   transition: function() { 
     return function(from, to, element) {
